465fc6d9a66e4bd1538754238eebcd5871a0227b
nvadim <nazarov-vadim@mail.ru>
2019-03-18 16:09:37 +0500

    refactoring CIblockElementDev class

--- CIblockElementDev.php
+++ CIblockElementDev.php
@@ -1,68 +1,59 @@
 <?php
+namespace Nvadim;
 
 use Bitrix\Main\Loader;
 use Bitrix\Main\Data\Cache;
-
-function d($var, $title = '')
-{
-        print '<pre style="width:9000;background: #000; color: #0f0; border: 1px solid #0f0;">';
-        if ($title != '') {
-            $ret
-                = '<p style="margin-top:-0px;background-color:#DD0000;font-size:17px;padding:5px 5px 5px 5px; border:1px solid green;color:black;font-weight:bold">'
-                . $title . '</p>';
-        }
-        $trace = debug_backtrace();
-        $ret .= $trace[0]['file'] . ':' . $trace[0]['line'] . '</br></br>';
-
-        $ret .= print_r($var, true);
-        $ret = str_replace('=>', '<font color="#ffffff">=></font>', $ret);
-        $ret = str_replace('[', '[ <font color="#FFFF00">', $ret);
-        $ret = str_replace(']', '</font> ]', $ret);
-
-        print $ret;
-        print '</pre>';
-        print "<hr>";
-}
-
+use Bitrix\Iblock\ElementTable;
 
 class CIblockElementDev
 {
     /**
      * Кешированный метод получения списка элементов
      *
-     * @param $sort
-     * @param $filter
-     * @param bool $groupBy
-     * @param bool $navParams
-     * @param bool $fields
-     * @param bool $cacheTime
+     * @param $params available keys:
+     * 		"select" => array of fields in the SELECT part of the query, aliases are possible in the form of "alias"=>"field"
+     * 		"filter" => array of filters in the WHERE part of the query in the form of "(condition)field"=>"value"
+     * 		"group" => array of fields in the GROUP BY part of the query
+     * 		"order" => array of fields in the ORDER BY part of the query in the form of "field"=>"asc|desc"
+     * 		"limit" => integer indicating maximum number of rows in the selection (like LIMIT n in MySql)
+     * 		"offset" => integer indicating first row number in the selection (like LIMIT n, 100 in MySql)
+     *		"runtime" => array of entity fields created dynamically
+     *
+     * @param $cacheParams available keys:
+     * 		"ttl" => время кеша
+     *      "initDir" => папка кеша
+     *
      * @return array|bool
      */
-    public static function getList($sort, $filter, $groupBy = false, $navParams = false,  $fields = false, $cacheTime = false)
+    public static function getList($params, $cacheParams)
     {
-        if (!is_array($sort) || empty($sort))
-            $sort = ['ID' => 'ASC'];
+        if (!is_array($params['order']) || empty($params['order']))
+            $params['order'] = ['ID' => 'ASC'];
 
-        if (!is_array($filter) || empty($filter))
+        if (!is_array($params['filter']) || empty($params['filter']))
             return false;
 
         $result = [];
 
-        if ($cacheTime)
-            $life_time = $cacheTime;
+        if ($cacheParams['ttl'])
+            $life_time = $cacheParams['ttl'];
         else
             $life_time = 86400;
 
-        $cache_id = "iblock_elements::" . json_encode([$sort, $filter, $fields, $cacheTime]);
+        $cache_id = "iblock_elements::" . json_encode(array_merge($params, $cacheParams));
 
         $obCache = Cache::createInstance();
-        if ($obCache->initCache($life_time, $cache_id, $sCachePath)) {
+        $initDir = false;
+        if($cacheParams['initDir']) {
+            $initDir = $cacheParams['initDir'];
+        }
+        if ($obCache->initCache($life_time, $cache_id, $initDir)) {
             $result = $obCache->getVars();
         } elseif ($obCache->startDataCache()) {
             Loader::includeModule("iblock");
 
-            $rs = \CIBlockElement::GetList($sort, $filter, $groupBy, $navParams, $fields);
-            while ($el = $rs->Fetch()) {
+            $elementList = ElementTable::getList($params);
+            while($el = $elementList->fetch()) {
                 $result[] = $el;
             }
 
